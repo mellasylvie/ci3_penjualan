d02b30c77786b0425ade2b31dc5c5c4a3b90d6dc
Mellania Permata Sylvie <58023306+mellasylvie@users.noreply.github.com>
2024-01-26 11:43:56 +0700

    Update Barang_model.php

--- models/Barang_model.php
+++ models/Barang_model.php
@@ -7,4 +7,14 @@ class Barang_model extends CI_Model
         // Perintah ambil data barang
         return $this->db->get("barang")->result_array();
     }
+
+    public function ambil_supplier()
+    {
+        return $this->db->get("supplier")->result_array();
+    }
+
+    public function tambah_barang($data)
+    {
+        return $this->db->insert('barang', $data);
+    }
 }
